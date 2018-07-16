e61e171997ac44964e2d052454c6449c2adef66d
rbareja72 <rbareja72@gmail.com>
2018-07-16 23:35:17 +0530

    added apis

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
+require('./config/config');
 const bodyParser = require('body-parser');
 const express = require('express');
+const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todos');
@@ -16,6 +19,78 @@ app.post('/todos', (req,res)=>{
     res.status(400).send(e);
   });
 });
+
+app.get('/todos', (req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{
+    console.log(e);
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id',(req, res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id',(req, res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((result)=>{
+    if(!result){
+      return res.status(404).send();
+    }
+    res.status(200).send(result);
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+  if(!ObjectID.isValid(id)){
+    console.log('Id not valid');
+  }
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id,{$set: body}, {new: true}).then((todo)=>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});
+
+app.post('/users',(req, res)=>{
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user.save().then((doc)=>{
+    res.send(doc);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+});
+
 
 app.listen(3000, ()=>{
   console.log('listening on port 3000');
